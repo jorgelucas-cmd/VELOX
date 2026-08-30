08ecd53153fe861d14b99937303550893e4591ce
jorgelucas-cmd <abestado1423@gmail.com>
2026-08-30 01:19:59 -0300

    atualização 30/08/2026

--- scripts.js
+++ scripts.js
@@ -1,16 +1,42 @@
 const formulario = document .querySelector(".formulario");
-const mascara = document.querySelector(".mascara-formulario");
-
-
+const mascara1 = document.querySelector(".mascara-formulario");
+const mascara2 = document.querySelector(".mascara-formfrural");
+const mascara3 = document.querySelector(".mascara-formrural");
+const fibraRural = document .querySelector(".formfrural");
+const radioRural = document .querySelector(".formrural");
 
 function fibra() {
     formulario.style.left = "50%";
     formulario.style.transform = "translateX(-50%)";
-    mascara.style.visibility = "visible";
+    mascara1.style.visibility = "visible";
 }
 
 function esconderform() {
     formulario.style.left = "-850px";
     formulario.style.transform = "translateX(0)";
-    mascara.style.visibility = "hidden";
+    mascara1.style.visibility = "hidden";
+}
+
+function  fibrarural() {
+    fibraRural.style.left = "50%";
+    fibraRural.style.transform = "translateX(-50%)";
+    mascara2.style.visibility = "visible";
+}
+
+function rural() {
+    radioRural.style.left = "50%";
+    radioRural.style.transform = "translateX(-50%)";
+    mascara3.style.visibility = "visible";
+}
+
+function esconderform1() {
+    fibraRural.style.left = "-850px";
+    fibraRural.style.transform = "translateX(0)";
+    mascara2.style.visibility = "hidden";
+}
+
+function esconderform2() {
+    radioRural.style.left = "-850px";
+    radioRural.style.transform = "translateX(0)";
+    mascara3.style.visibility = "hidden";
 }
